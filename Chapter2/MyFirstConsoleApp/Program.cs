026ae3d7db048cb93ba62d52c5536e9b29961f06
Mariusz Krzak <mariusz.krzak84@gmail.com>
2023-12-25 19:27:37 +0100

    add myFirstConsoleApp with basic operator examples

---
+++ Chapter2/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            string result = "Powierzchnia";
+            result = result + " wynosi " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+
+
+            int someValue = 10;
+            string message = "";
+
+            if (someValue==24)
+            {
+                message = "Wartość wynosi 24!";
+            }
+            else
+            {
+                message = "Wartość nie wynosi 24";
+            }
+
+        }
+    }
+}
