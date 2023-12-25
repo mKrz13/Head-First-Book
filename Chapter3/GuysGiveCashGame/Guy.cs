7aed1c9dfd7efb4256125a876ac836140d659a09
Mariusz Krzak <mariusz.krzak84@gmail.com>
2023-12-25 23:45:45 +0100

    Add GuysGiveCashGame

---
+++ Chapter3/GuysGiveCashGame/Guy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuysGiveCashGame
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+
+
+        /// <summary>
+        /// Wyświetla w konsoli imię i posiadanąkwotę.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"{Name} ma {Cash} zł.");
+        }
+
+
+
+        /// <summary>
+        /// Przekazuje pieniądze i usuwa je z portwela (lub wyświetla w konsoli komunikat o braku środków
+        /// </summary>
+        /// <param name="amount">Przekazywana kwota</param>
+        /// <returns>Ilość pieniędzy wyjmowanych z portwela lub 0, jeśli dostępne środki są za małe. (lub parametr amount jest nieprawidłowy). </returns>
+
+
+        public int GiveCash(int amount)
+        {
+            if (amount<=0)
+            {
+                Console.WriteLine($"{Name} mówi: {amount} nie jest poproawną kwotą.");
+                return 0;
+            }
+            if (amount>Cash)
+            {
+                Console.WriteLine($"{Name} mówi: nie mam wystarczających środków aby dać Ci {amount} zł.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+
+        public void ReceiveCash (int amount)
+        {
+            if (amount<=0)
+            {
+                Console.WriteLine($"{Name} mówi: nie przyjmę {amount} zł.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Chapter3/GuysGiveCashGame/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GuysGiveCashGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = .75;
+            Guy player = new Guy() { Name = "Gracz", Cash = 100 };
+
+            Console.WriteLine($"Witaj w kasynie. Prawdopodobieństwo przegranej: {odds}.");
+
+            while (player.Cash>0)
+            {
+                player.WriteMyInfo();
+                Console.Write("Stawiana kwota: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount)*2;
+                    double number = random.NextDouble();
+                    if (pot > 0)
+                    {
+                        if (number > odds)
+                        {
+                            player.ReceiveCash(pot);
+                            Console.WriteLine($"Wygrałeś {pot}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Niestety przegrałeś.");
+                        }
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Niestety przegrałeś.\nKasyno zawsze wygrywa.");
+
+
+
+        }
+    }
+}
