1c20e043cb1eaf1f13094bfc0806879089095b46
Mariusz Krzak <mariusz.krzak84@gmail.com>
2023-12-27 11:35:04 +0100

    add RandomClassExamples app

---
+++ Chapter4/RandomClassExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RandomClassExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomInt = random.Next();
+            Console.WriteLine(randomInt);
+            int zeroToNine = random.Next(10);
+            Console.WriteLine(zeroToNine);
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine(dieRoll);
+            double randomDouble = random.NextDouble();
+            Console.WriteLine(randomDouble * 100);
+            Console.WriteLine((float)randomDouble*100f);
+            Console.WriteLine((decimal)randomDouble*100m);
+            int zeroOrOne = random.Next(2);
+            bool coinFlip = Convert.ToBoolean(zeroOrOne);
+            Console.WriteLine(coinFlip);
+        }
+    }
+}
