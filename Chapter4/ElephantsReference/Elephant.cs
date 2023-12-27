b30f52d0e3a6a98c3fa86257bbcebd408b093878
Mariusz Krzak <mariusz.krzak84@gmail.com>
2023-12-27 11:19:03 +0100

    add ElephantsReference app

---
+++ Chapter4/ElephantsReference/Elephant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElephantsReference
+{
+    internal class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"Na imię mam {Name}.\nDługość moich uszu to {EarSize} cm.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine($"{Name} usłyszał wiadomość.");
+            Console.WriteLine($"{whoSaidIt.Name} powiedział(a) {message}.");
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+
+
+    }
+}
---
+++ Chapter4/ElephantsReference/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ElephantsReference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucek = new Elephant() { Name = "Lucek", EarSize = 100};
+            Elephant laura = new Elephant() { Name = "Laura", EarSize = 80};
+
+
+            Console.WriteLine("Wciśnij 1 (Lucek), 2 (Laura) lub 3 (przestawienie):");
+
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine($"Wcisnąłeś {input}.");
+                if (input=='1')
+                {
+                    Console.WriteLine("Wywołanie lucek.WhoAmI().");
+                    lucek.WhoAmI();
+                }else if (input == '2')
+                {
+                    Console.WriteLine("Wywołanie laura.WhoAmI().");
+                    laura.WhoAmI();
+                }else if(input == '3')
+                {
+                    Elephant elephantHolder = lucek;
+                    lucek = laura;
+                    laura = elephantHolder;
+                    Console.WriteLine("Referencje zostały przestawione");
+                }
+                else if (input=='4')
+                {
+                    lucek = laura;
+                    lucek.EarSize = 4321;
+                    lucek.WhoAmI();
+                }
+                else if (input=='5')
+                {
+                    laura.SpeakTo(lucek, "Cześć, lucku");
+                }
+                else
+                {
+                    Console.WriteLine("Zamknięcie programu");
+                    break;
+                }
+            }
+
+        }
+    }
+}
