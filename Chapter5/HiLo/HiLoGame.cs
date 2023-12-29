05e016f1f5d44635ce9641f57ca9ebebbbbd9b0f
Mariusz Krzak <mariusz.krzak84@gmail.com>
2023-12-29 11:44:29 +0100

    add HiLo console app

---
+++ Chapter5/HiLo/HiLoGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HiLo
+{
+    internal static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+
+
+
+
+        internal static int GetPot()
+        {
+            return pot;
+        }
+
+        internal static void Guess(bool higher)
+        {
+            int nextNumber = random.Next(1, MAXIMUM + 1);
+            if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber)) { Console.WriteLine("Zgadłeś!"); pot++; }
+            else { Console.WriteLine("Niestety, błędna odpowiedź"); pot--; }
+            currentNumber = nextNumber;
+            Console.WriteLine($"Aktualna wartość to: {currentNumber}.");
+        }
+
+        internal static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber>=half) Console.WriteLine($"Liczba wynosi co najmniej: {half}.");
+            else Console.WriteLine($"Liczba jest mniejsza niż: {half}.");
+            pot--;
+
+        }
+    }
+}
---
+++ Chapter5/HiLo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HiLo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w HiLo");
+            Console.WriteLine($"Odgadnij liczbę od 1 do {HiLoGame.MAXIMUM}");
+            HiLoGame.Hint();
+            while (HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine("Wciśnij w (większa), m (mniejsza), lub ? (kupno wskazówki).");
+                Console.WriteLine($"Inne klawisze to koniec gry z {HiLoGame.GetPot()} zł.");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'w') HiLoGame.Guess(true);
+                else if (key == 'm') HiLoGame.Guess(false);
+                else if (key == '?') HiLoGame.Hint();
+                else return;
+            }
+            Console.WriteLine("Skończyły Ci się pieniądze. Do zobaczenia!");
+
+        }
+    }
+}
