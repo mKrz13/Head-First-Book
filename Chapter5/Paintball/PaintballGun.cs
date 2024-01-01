efe20ba635758ca2c7df05b72b8ef494e9ea65d5
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-01-01 20:51:35 +0100

    add Painball console app

---
+++ Chapter5/Paintball/PaintballGun.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paintball
+{
+    internal class PaintballGun
+    {
+
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+                
+        }
+
+
+        public int MagazineSize { get; private set; }
+
+        private int balls = 0;
+
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() { return balls == 0; }
+
+        public int Balls 
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0) balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MagazineSize) BallsLoaded = MagazineSize;
+            else BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0 ) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ Chapter5/Paintball/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Paintball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBalls = ReadInt(20, "Liczba kulek");
+            int magazineSize = ReadInt(16, "Pojemność magazynka");
+            Console.WriteLine($"Załadowany [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+
+
+            PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
+            while (true)
+            {
+                Console.WriteLine($"Kulki : {gun.Balls}, załadowano: {gun.BallsLoaded}");
+                if (gun.IsEmpty()) Console.WriteLine("Brak amunicji");
+                Console.WriteLine("Spacja - strzał, p - przeładowanie, + - dodaj amunicję, k - koniec");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Próba strzału: {gun.Shoot()}");
+                else if (key == 'p') gun.Reload();
+                else if (key == '+') gun.Balls += gun.MagazineSize;
+                else if (key == 'k') return;
+
+            }
+        }
+
+        private static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write($"{prompt} + [{lastUsedValue}]: ");
+            if (int.TryParse(Console.ReadLine(), out int value))
+            {
+                Console.WriteLine($"\t użycie wartości {value}");
+                return value;
+            }
+            else
+            {
+                Console.WriteLine($"\t użycie wartości domyślnej {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
+    }
+}
