746e0a880a02ffabc59978ad5b51a929f53b8291
Mariusz Krzak <mariusz.krzak84@gmail.com>
2024-01-01 21:55:17 +0100

    add OscarDamageCalculatorHerm console app

---
+++ Chapter5/OscarDamageCalculatorHerm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace OscarDamageCalculatorHerm
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage(RollDice());
+
+            while (true)
+            {
+                Console.Write($"0 - ani magiczny, ani płonący; 1 - magiczny; 2 - płonący; 3 - magiczny i płonący; inne wartości - koniec: ");
+                char key = Console.ReadKey().KeyChar;
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+                swordDamage.Roll = RollDice();
+                swordDamage.Magic = (key == '1' || key == '3');
+                swordDamage.Flaming = (key == '2' || key == '3');
+                Console.WriteLine($"\nRzut: {swordDamage.Roll}, punkty obrażeń: {swordDamage.Damage}\n");
+            }
+        }
+
+        private static int RollDice()
+        {
+            return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+
+        }
+    }
+}
---
+++ Chapter5/OscarDamageCalculatorHerm/SwordDamage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscarDamageCalculatorHerm
+{
+    internal class SwordDamage
+    {
+
+        public SwordDamage(int roll)
+        {
+            this.roll = roll;
+            CalculateDamage();
+        }
+
+        /// <summary>
+        /// Value always added to damage.
+        /// </summary>
+        public const int BASE_DAMAGE = 3;
+
+        /// <summary>
+        /// Value added to damage if sword is flaming.
+        /// </summary>
+        public const int FLAME_DAMAGE = 2;
+
+        private int roll;
+        /// <summary>
+        /// 3d6 roll value to count damage.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+        /// <summary>
+        /// Is sword flaming.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Is sword magic.
+        /// </summary>
+        private bool magic;
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Damage delt with sword.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        /// <summary>
+        /// Used to calculate damage. If sword is magic multiply roll by 1,75. If sword is flaming add FLAME+DAMAGE;
+        /// </summary>
+        public void CalculateDamage()
+        {
+
+            decimal magicMultiplier = 1;
+            if (magic) magicMultiplier = 1.75m;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (flaming) Damage += FLAME_DAMAGE;
+        }
+
+
+
+
+
+
+    }
+}
